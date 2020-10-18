606667c8538bcf46ad753f4a3e66fc5d00fcd77c
Boróka File <boroka.file@stud.uni-corvinus.hu>
2020-10-18 20:53:53 +0200

    4

--- 5.het/5.het/Form1.cs
+++ 5.het/5.het/Form1.cs
@@ -12,9 +12,15 @@ namespace _5.het
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+      
         public Form1()
         {
+            
             InitializeComponent();
+            Ticks = context.Tick.ToList();
+           dataGridView1.DataSource = Ticks;
         }
     }
 }
