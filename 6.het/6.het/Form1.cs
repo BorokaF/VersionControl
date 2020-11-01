6ecb4408430d2b5df2ead8fd6e3b5635310c7b5f
Boróka File <boroka.file@stud.uni-corvinus.hu>
2020-11-01 19:39:30 +0100

    2-3

--- 6.het/6.het/Form1.cs
+++ 6.het/6.het/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _6.het.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,25 @@ namespace _6.het
 {
     public partial class Form1 : Form
     {
+       
         public Form1()
         {
-            InitializeComponent();
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+           // InitializeComponent();
+
         }
     }
 }
