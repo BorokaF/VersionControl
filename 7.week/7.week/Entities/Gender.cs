1fb488e5a5c1a52a15c963318c3c33534d3ca1ef
Boróka File <boroka.file@stud.uni-corvinus.hu>
2020-11-08 19:21:56 +0100

    2

---
+++ 7.week/7.week/Entities/Gender.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.week.Entities
+{
+    public enum Gender
+    {
+
+        Male = 1,
+        Female = 2
+    }
+
+    public class Person
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+        public bool IsAlive { get; set; }
+
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double BProb { get; set; }
+
+    }
+
+    public class DeathProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double DProb { get; set; }
+
+    }
+
+}
