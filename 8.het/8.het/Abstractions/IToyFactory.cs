2e5adcd1df0ed4afa8ce307aa9a7b8a32d6803ce
Boróka File <boroka.file@stud.uni-corvinus.hu>
2020-11-22 21:00:10 +0100

    10

--- 8.het/8.het/Abstractions/IToyFactory.cs
+++ 8.het/8.het/Abstractions/IToyFactory.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _8.het.Entities;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace _8.het.Abstractions
 {
-    interface IToyFactory
+   public interface IToyFactory
     {
         Toy CreateNew();
     }
--- 8.het/8.het/Entities/CarFactory.cs
+++ 8.het/8.het/Entities/CarFactory.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Drawing;
 
 namespace _8.het.Entities
 {
-   public class BallFactory : IToyFactory
+    class CarFactory : IToyFactory
     {
+       
+    
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
