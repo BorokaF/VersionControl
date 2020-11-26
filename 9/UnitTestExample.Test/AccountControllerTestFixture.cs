628e5fa5aaca23a9099cd0df65daa1f9fd122def
Boróka File <boroka.file@stud.uni-corvinus.hu>
2020-11-27 00:41:06 +0100

    7-8

--- 9/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ 9/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -14,7 +14,10 @@ namespace UnitTestExample.Test
     TestCase("abcd1234", false),
     TestCase("irf@uni-corvinus", false),
     TestCase("irf.uni-corvinus.hu", false),
-    TestCase("irf@uni-corvinus.hu", true)]
+    TestCase("irf@uni-corvinus.hu", true)
+          ]
+        
+
         public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
@@ -27,7 +30,29 @@ namespace UnitTestExample.Test
             Assert.AreEqual(expectedResult, actualResult);
 
         }
-        
+        [Test, 
+            TestCase("abcd1234", false),
+             TestCase("Abcd1234", true),
+            TestCase("1234", false),
+            TestCase("Abcd", false),
+            TestCase("Abcdefghijk", false),
+            TestCase("ABCDEFGHI", false),
+            TestCase("abcdefhjknf", false)]
 
-    }
+        //   A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+
+            var accountController = new AccountController();
+
+
+            var actualResult = accountController.ValidatePassword(password);
+
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+        
+        
+    } 
 }
