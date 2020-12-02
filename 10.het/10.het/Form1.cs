a3e7b47cb080366afaef21afb36aa843337a5b7c
Boróka File <boroka.file@stud.uni-corvinus.hu>
2020-12-02 20:55:27 +0100

    5

--- 10.het/10.het/Form1.cs
+++ 10.het/10.het/Form1.cs
@@ -26,10 +26,33 @@ namespace _10.het
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-            gc.AddPlayer(populationSize);
+            /*gc.AddPlayer(populationSize);
             gc.AddPlayer(nbrOfSteps);
            
+            gc.Start();*/
+            gc.GameOver += Gc_GameOver;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
             gc.Start();
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+        }
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+        }
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
-}
+    }
